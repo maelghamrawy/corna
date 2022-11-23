6fb5fdfd677db216a2e3f37fa8903aa54f06d4f3
maelghamrawy <maelghamrawy@gmail.com>
2022-11-23 23:36:34 +0200

    finish navBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import logo from "../image/logo.webp"
+import { Link } from 'react-scroll'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
     return (
-
+<>
         <nav className="navbar navbar-expand-lg navbar-light bg-white ">
-            <div className="container-fluid ">
+            <div className="container ">
 
-                <img className="" style={{width: "120px" ,marginLeft: "200px", marginRight: "100px"}} src={logo} href="logo"/>
+                <img className="" style={{width: "100px"}} src={logo} href="logo"/>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -16,37 +17,42 @@ const NavBar = () => {
 
                 <div className="collapse navbar-collapse" id="navbarNav">
 
-                    <ul className="navbar-nav " >
+                    <ul className=" navbar-nav  justify-content-between  " >
                         <li className="nav-item" >
-                            <a className="nav-link active text-secondary" aria-current="page" href="home">Home</a>
+                            <Link className="nav-link active text-secondary" aria-current="page" href="home" activeClass="active" smooth spy to="about">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-secondary" href="home">AboutCorona</a>
+                            <Link className="nav-link text-secondary" href="home" activeClass="active" smooth spy to="about">AboutCorona</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-secondary" href="home">Symptoms</a>
+                            <Link className="nav-link text-secondary" href="home" activeClass="active" smooth spy to="about">Symptoms</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link  text-secondary " href="home" tabindex="-1" aria-disabled="true">Prevention</a>
+                            <Link className="nav-link  text-secondary " href="home" tabindex="-1" aria-disabled="true" activeClass="active" smooth spy to="about">Prevention</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true">Treatment</a>
+                            <Link className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true" activeClass="active" smooth spy to="about">Treatment</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true">FAQ</a>
+                            <Link className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true" activeClass="active" smooth spy to="about">FAQ</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true">News</a>
+                            <Link className="nav-link  text-secondary" href="home" tabindex="-1" aria-disabled="true" activeClass="active" smooth spy to="about">News</Link>
                         </li>
                         <li className="nav-item bg-primary rounded-pill p-1 ">
-                            <a className="nav-link  text-secondary " href="home" tabindex="-1" aria-disabled="true">Do $ Don't</a>
+                            <Link className="nav-link  text-secondary " href="home" tabindex="-1" aria-disabled="true" activeClass="active" smooth spy to="about">Do $ Don't</Link>
                         </li>
                     </ul>
 
                 </div>
             </div>
+       
         </nav>
-
+        <section id="about">ABOUT</section>
+         <section id="projects"> project</section>
+        <section id="blog1">BLOG</section>
+        <section id="contact">CONTACT ME</section>
+</>
     )
 }
 
